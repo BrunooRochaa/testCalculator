78ed415e310343c03a8e31f7d2c76ec15fe841f5
Bruno Rocha <brunoorochaa@gmail.com>
2019-10-09 19:07:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function resultado() {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    let op = document.getElementById('op').value;
+
+    num1 = Number(num1)
+    num2 = Number(num2)
+    
+    if (op == '+'){
+        res = num1 + num2
+    } else if (op == '-'){
+        res = num1 - num2
+    } else if (op == '/'){
+        res = num1 / num2
+    }else{
+        res = num1 * num2
+    }
+    restxt = document.getElementById('res');
+    restxt.innerHTML = res
+
+}
