6dee0c9afe8c3fbb0fa57665d0a655486725ab98
Bruno Rocha <brunoorochaa@gmail.com>
2019-10-10 17:36:25 -0300

    finish the plus operator; result appear on screen

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # testCalculator
 Simple calculator to learn JS and how git works
+
+# Calculator v1
+
+- [x] Done
+
+# Calculator v2
+
+To do
+- [x] make result appear in visor
+- [] finish all operations (for now just '+' works)
+- [] make a clear button
---
+++ calculator-v2/index.js
@@ -0,0 +1,18 @@
+function visor(num){
+    document.getElementById('display').value = document.getElementById('display').value + num;
+}
+
+function calc() {
+    conta = document.getElementById('display').value;
+    
+    if (conta.indexOf('+') > 0) {
+        numString = conta.split('+');
+
+        num1 = Number(numString[0]);
+        num2 = Number(numString[1]);
+
+        res = num1 + num2;
+        
+        document.getElementById('display').value = `${res}`;/
+    }
+}
